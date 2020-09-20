342491f5986e070fab36c37f638a80038440193f
Bill Riehl <wjriehl@lbl.gov>
2020-09-20 14:49:15 -0700

    added a simple motor controller

---
+++ motor.py
@@ -0,0 +1,82 @@
+import RPi.GPIO as GPIO
+import time
+
+STEPS = [
+    [1, 0, 0, 0],
+    [1, 1, 0, 0],
+    [0, 1, 0, 0],
+    [0, 1, 1, 0],
+    [0, 0, 1, 0],
+    [0, 0, 1, 1],
+    [0, 0, 0, 1],
+    [1, 0, 0, 1]
+]
+
+CLOCKWISE = True
+COUNTERCLOCKWISE = False
+
+# in1 = 11
+# in2 = 13
+# in3 = 15
+# in4 = 16
+# chans = [in1, in2, in3, in4]
+
+
+# GPIO.setup([in1, in2, in3, in4], GPIO.OUT)
+step = 0
+direction = True  # True = clockwise, False = counterclockwise
+
+class StepperMotor:
+    """
+    Provides an interface for the 28BYJ-48 stepper motor.
+    Usage:
+    motor = StepperMotor(pin1, pin2, pin3, pin4)
+    motor.run(direction, num_steps)
+    motor.shutdown()
+
+    pin1 through pin4 match up with the IN1 through IN4 on the motor driver board
+    Initializing a StepperMotor class will claim control of the pins.
+    It's best to run motor.shutdown() before ending your program.
+    """
+    def __init__(self, pin1: int, pin2: int, pin3: int, pin4: int):
+        self.pins = [pin1, pin2, pin3, pin4]
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(self.pins, GPIO.OUT)
+
+    def _get_next_step(self, step: int, d: bool) -> int:
+        step += 1 if d == CLOCKWISE else -1
+        if step > 7:
+            return 0
+        elif step < 0:
+            return 7
+        else:
+            return step
+
+    def _do_step(self, step: int):
+        if step < 0 or step > 7:
+            GPIO.output(self.pins, [0, 0, 0, 0])
+        else:
+            GPIO.output(self.pins, STEPS[step])
+
+    def run(self, steps: int, direction: bool, speed: float):
+        """
+        Run the motor in a direction for a certain number of steps with
+        some speed (time between steps).
+        e.g.
+        motor.run(CLOCKWISE, 4000, 0.01)
+        """
+        if steps <= 0:
+            return
+        cur_step = 0
+        for i in range(steps):
+            self._do_step(cur_step)
+            cur_step = self._get_next_step(cur_step, direction)
+            time.sleep(speed)
+
+    def shutdown(self):
+        GPIO.cleanup()
+
+if __name__ == "__main__":
+    motor = StepperMotor(11, 13, 15, 16)
+    motor.run(4000, CLOCKWISE, 0.001)
+    motor.shutdown()
--- notes.md
+++ notes.md
@@ -31,7 +31,7 @@ Once installed and configured with `raspi-config` I set it up with the following
     1.  Move the `theater.service` to the systemd directory - in the version of the OS I used
 
             sudo mv theater.service /etc/systemd/system
-    2.  reload the `systemd` daemon: 
+    2.  reload the `systemd` daemon:
 
             sudo systemctl daemon-reload
     3.  enable the service:
@@ -45,3 +45,19 @@ Now that part's set - the Raspberry Pi will run the `theater.py` script on start
 1.  Add a switch or button to not load the script in some mode. A debug toggle.
 2.  Learn the GPIO pins in Raspberry Pi and how to manipulate them to do things like control a motor.
 3.  Come up with a setup or stand for the screen that's separate from the Raspberry Pi. I think the motor will have to run from the RPi power pins, then I'll just plug the monitor and Pi in separately.
+
+## GPIO stuff
+I think, due to how the pins work on the Pi and the screen, It won't work with the screen plugged directly into the Pi to make a sandwich shape. I mean, it's fine if we don't want to have the Pi control the motor to the curtains, but we do.
+
+So.
+
+Our options are.
+
+1.  Plug in the screen.
+    1.  Don't do the curtain thing at all, maybe animate some on the screen.
+    2.  Be clever with attaching pins to the Pi with the screen attached, get 5V power from somewhere else.
+    3.  Be really clever and possibly not fire safe and use the 5V rail on the Pi. It might overtax it, though. We'll see.
+2.  Don't plug in the screen. Need a separate power source for the screen and a wee baby HDMI cable.
+    1. Freely use all the Pi pins.
+    2. One 5V -> motor, other (maybe) can power some lights? So now the lights go out, and the curtains are drawn, then the show begins. And the lights go back on at the end. Neat.
+    3. Just the motor.
